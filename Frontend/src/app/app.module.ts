afb489d76a6b61f1235d1efd8748c632c06b71fd
SankalpaGunasekara <sankalpa.gunasekara99@gmail.com>
2024-05-29 11:19:05 +0530

    essential packages installed

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -8,6 +8,7 @@ import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { PriceCalculatorComponent } from './price-calculator/price-calculator.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -20,7 +21,8 @@ import { PriceCalculatorComponent } from './price-calculator/price-calculator.co
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
