d93fd6d112c6d5fb081500779e96e58c95cb094b
SankalpaGunasekara <sankalpa.gunasekara99@gmail.com>
2024-05-29 11:42:30 +0530

    init commit of backend

---
+++ Backend/src/test/java/com/example/VendorCalc/VendorCalcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.VendorCalc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VendorCalcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
