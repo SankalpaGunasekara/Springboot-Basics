abc39db400e2810a3b67a18bfd4c449c5dfecc6e
SankalpaGunasekara <sankalpa.gunasekara99@gmail.com>
2024-05-29 11:12:16 +0530

    main components created

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductListComponent } from './product-list/product-list.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { PriceCalculatorComponent } from './price-calculator/price-calculator.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductListComponent,
+    NavBarComponent,
+    HeaderComponent,
+    FooterComponent,
+    PriceCalculatorComponent
   ],
   imports: [
     BrowserModule,
